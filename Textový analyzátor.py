27f98d33609f77c0466a202971d6c20b92800628
Tomáš Plechatý <tomplechaty@seznam.cz>
2024-06-03 14:16:07 +0200

    Ošetřena možnost nečíselného vstupu při výběru 1-3

--- Textový analyzátor.py
+++ Textový analyzátor.py
@@ -26,65 +26,71 @@ if verified:
     print("We have", len(TEXTS),"texts to be analyzed.")
     print(40*"-")
     choice = input("Enter a number btw. 1 and 3 to select: ")
-    choice = int(choice)
+    
     print(40*"-")
     
-    if not int(choice) in range(1,4):
-        print("invalid choice, terminejtink D program..")
+    if not choice.isnumeric():
+        print(f"Choice must be only number from 1 to 3")
     else:
-        source = TEXTS[choice-1].replace(",", "")
-        source = source.replace(".", "")
-        word_counts = len(TEXTS[choice-1].split())
-        title_case = 0
-        upper_case = 0
-        lower_case = 0
-        numeric_strings = 0
-        sum_of_all_numbers = 0
-        dict_of_lenghts = {}
-        max_len = 0
+        choice = int(choice)
+        if not int(choice) in range(1,4):
+            print("Invalid choice, terminejtink D program..")
 
-        for word in source.split():
-            if len(word) > max_len:
-                max_len = len(word)
     
-        for lenght in range(1,max_len+1):
-            dict_of_lenghts[lenght] = 0
+        else:
+            source = TEXTS[choice-1].replace(",", "")
+            source = source.replace(".", "")
+            word_counts = len(TEXTS[choice-1].split())
+            title_case = 0
+            upper_case = 0
+            lower_case = 0
+            numeric_strings = 0
+            sum_of_all_numbers = 0
+            dict_of_lenghts = {}
+            max_len = 0
+
+            for word in source.split():
+                if len(word) > max_len:
+                    max_len = len(word)
     
-        for word in source.split():
+            for lenght in range(1,max_len+1):
+                dict_of_lenghts[lenght] = 0
+    
+            for word in source.split():
                 
-            if  not word.isnumeric() and word == word.title():
-                title_case += 1
-            if word.isalpha() and word.islower():
-                lower_case += 1
-            if word.isalpha() and word.isupper():
-                upper_case += 1
-            if word.isnumeric():
-                numeric_strings +=1
-                word_value = word
-                word_value = int(word_value)
-                sum_of_all_numbers +=word_value
+                if  not word.isnumeric() and word == word.title():
+                    title_case += 1
+                if word.isalpha() and word.islower():
+                    lower_case += 1
+                if word.isalpha() and word.isupper():
+                    upper_case += 1
+                if word.isnumeric():
+                    numeric_strings +=1
+                    word_value = word
+                    word_value = int(word_value)
+                    sum_of_all_numbers +=word_value
         
-            word_lenght = len(word)
-            dict_of_lenghts[word_lenght] +=1
-        max_value = 0
-        for value in dict_of_lenghts:
-            if dict_of_lenghts[value]>max_value:
-                max_value = dict_of_lenghts[value]
+                word_lenght = len(word)
+                dict_of_lenghts[word_lenght] +=1
+            max_value = 0
+            for value in dict_of_lenghts:
+                if dict_of_lenghts[value]>max_value:
+                    max_value = dict_of_lenghts[value]
         
-        print("There are",word_counts,"words in the selected text.")
-        print("There are",title_case,"titlecase words." ) 
-        print("There are",upper_case,"uppercase words." ) 
-        print("There are",lower_case,"lowercase words." ) 
-        print("There are", numeric_strings,"numeric strings.")
-        print("The sum of all the numbers", sum_of_all_numbers)
-        print(40*"-")
-        print("LEN|  OCCURENCES  |NR.")
-        print(40*"-")
-        for lenght in dict_of_lenghts:
-            if lenght < 10:
-                print(3*" ",lenght,"|",dict_of_lenghts[lenght]*"*",(max_value+2-dict_of_lenghts[lenght])*" ","|",dict_of_lenghts[lenght],sep="")
-            if lenght > 9 and lenght < 100 :
-                print(2*" ",lenght,"|",dict_of_lenghts[lenght]*"*",(max_value+2-dict_of_lenghts[lenght])*" ","|",dict_of_lenghts[lenght],sep="")    
+            print("There are",word_counts,"words in the selected text.")
+            print("There are",title_case,"titlecase words." ) 
+            print("There are",upper_case,"uppercase words." ) 
+            print("There are",lower_case,"lowercase words." ) 
+            print("There are", numeric_strings,"numeric strings.")
+            print("The sum of all the numbers", sum_of_all_numbers)
+            print(40*"-")
+            print("LEN|  OCCURENCES  |NR.")
+            print(40*"-")
+            for lenght in dict_of_lenghts:
+                if lenght < 10:
+                    print(3*" ",lenght,"|",dict_of_lenghts[lenght]*"*",(max_value+2-dict_of_lenghts[lenght])*" ","|",dict_of_lenghts[lenght],sep="")
+                if lenght > 9 and lenght < 100 :
+                    print(2*" ",lenght,"|",dict_of_lenghts[lenght]*"*",(max_value+2-dict_of_lenghts[lenght])*" ","|",dict_of_lenghts[lenght],sep="")    
 
 else:
     print("unregistered user, terminating the program..")
